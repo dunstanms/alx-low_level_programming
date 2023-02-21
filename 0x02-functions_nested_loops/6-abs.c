25cd10810069fce017f8fb6ede61f9e68de7902a
ubuntustan <dunmmbehero@gmail.com>
2023-02-21 11:45:21 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - compute the absolute value of an integer
+ * @n: int to be checked
+ * Return: the absolute int value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
