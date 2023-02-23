8942e0f612d2207cee1da32d26852473f71f59e1
ubuntustan <dunmmbehero@gmail.com>
2023-02-23 10:18:14 -0800

    task1 -9

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: int to multiply to b
+ * @b: int to multiply to a
+ * Return: operation result
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from zero to nine
+ * followed by new line
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c < 10; i++)
+	{
+		_putchar(c + '0');
+	}
+
+	_putchar('\n');
+}
