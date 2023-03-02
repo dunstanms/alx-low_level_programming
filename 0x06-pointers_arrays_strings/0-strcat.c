6162390945b0ff1d4186d553d01ad64d052c66e6
ubuntustan <dunmmbehero@gmail.com>
2023-03-02 06:40:10 -0800

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strcat - appends the src string to dest string
+ * overwriting the terminal null byte at end of dest
+ *and then adds terminal bite
+ * @dest: string to append to
+ * @src: string to add
+ *
+ * Return:a pointer to the final string output
+ */
+char *_strcat(char *dest, char *src)
+{
+	int d, s;
+
+	d = 0;
+	s = 0;
+
+	while (dest[d] != '\0')
+	{
+		dest[d] = src[s];
+		d++;
+		s++;
+	}
+
+	dest[d] = '\0';
+
+	return (dest);
+}
